d31822ac6fe84fd2ec199c130f36a32c5912381d
Yarden <38938773+Yarden84@users.noreply.github.com>
2025-03-07 08:44:34 +0100

    clean up

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { apiRoutes } = require('./routes/apiRoutes');
-// const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,11 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Use routes
 app.use('/api', apiRoutes);
-
-// Error handling middleware
-// app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
