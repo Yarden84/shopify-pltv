3c00f97a6be0ca69ab385efc8550e27bd75dfc3b
Yarden <38938773+Yarden84@users.noreply.github.com>
2025-03-07 17:57:00 +0100

    puppeteer config update

--- server/puppeteer.config.cjs
+++ server/puppeteer.config.cjs
@@ -1,5 +1,9 @@
 const { join } = require('path');
+
+// Use a path relative to the project root or adjust based on the environment
+const cachePath = process.env.NODE_ENV === 'production' ? '/opt/render/.cache/puppeteer' : join(__dirname, '.cache', 'puppeteer');
+
 /**
 * @type {import("puppeteer").Configuration}
 */
-module.exports = { cacheDirectory: join(__dirname, '.cache', 'puppeteer') };
+module.exports = { cacheDirectory: cachePath };
