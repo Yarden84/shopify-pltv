88ae335b310886afd3c0b1f3b2a87a9dee90f878
Yarden <38938773+Yarden84@users.noreply.github.com>
2025-03-07 18:25:31 +0100

    delete config

--- server/puppeteer.config.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-// Use a path relative to the project root or adjust based on the environment
-const cachePath = process.env.NODE_ENV === 'production' ? '/opt/render/.cache/puppeteer' : join(__dirname, '.cache', 'puppeteer');
-
-/**
-* @type {import("puppeteer").Configuration}
-*/
-module.exports = { cacheDirectory: cachePath };
